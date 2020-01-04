af56aa276b192f6a371b4a103def62dede06bbbf
Cris Stringfellow <3038633-dosycorp@users.noreply.gitlab.com>
2020-01-04 00:09:49 -0600

    "good"

--- c.js
+++ c.js
@@ -2,26 +2,93 @@
 
 const o = null;
 const {body} = document;
-const myText = 'Cris Stringfellow is Fucking Awesome';
 const myStyle = {
   color: '#b0b0b0',
   background: 'linear-gradient(to right, lime, dodgerblue)',
   margin: 0
 };
-C`h1 ${myText} ${myStyle}`(body);
-const list = C`ul ${o} ${myStyle}`(body);
-C`li ${myText} ${myStyle}`(list);
-C`li ${myText} ${myStyle}`(list);
-C`li ${myText} ${myStyle}`(list);
+
+{
+  const f = C`form ${o} ${myStyle}`(body);
+  const l = C`p label ${"Name"}`(f);
+  C`input ${{
+    type: 'text',
+    placeholder: 'your name'
+  }}`(l);
+}
+
+{
+  C`input ${{
+    type: 'text',
+    placeholder: 'your name'
+  }}`(C`p label ${"Name"}`(C`form ${o} ${myStyle}`(body)));
+}
+
+{
+  w`form ${o} ${myStyle} p label ${"Name"} input ${{type:'text', placeholder:'your name'}}`(body);
+}
 
 
-function C([tag], text, style) {
-  tag = tag.trim();
-  z = document.createElement(tag);
-  z.innerText = text;
-  Object.assign(z.style, style);
-  return point => {
-    point.insertAdjacentElement('beforeEnd', z);
-    return z;
-  };
+function C([tag], content = '', style = {}) {
+  if ( ! tag ) {
+    throw new TypeError("Must specify tag");
+  }
+
+  // make tag stack
+    tag = tag.trim().split(/\s+/g).filter(t  => t.length);
+    let z = document.createElement(tag.shift());
+    for ( const t of tag ) {
+      z = z.appendChild(document.createElement(t));
+    }
+
+
+  // insert local content at stack top
+    if ( content == null || content == undefined ) {
+      // do nothing
+    } else if ( typeof content == "string" ) {
+      z.innerText = content;
+    } else if ( typeof content == "object" ) {
+      Object.keys(content).forEach(attrName => {
+        const attrValue = content[attrName];
+        try {
+          z.setAttribute(attrName, attrValue);
+        } catch(e) {}
+        try {
+          z[attrName] = attrValue;
+        } catch(e) {}
+      });
+    }
+
+  // apply style
+    Object.assign(z.style, style);
+
+  // return inserter function
+    return point => {
+      if ( ! point ) {
+        throw new TypeError("Must provide an insertion point to the function call.");
+      }
+      point.insertAdjacentElement('beforeEnd', z);
+      return z;
+    };
+}
+
+function w(tags, params) {
+  let m, M;
+  while( hasSlice(tags, params) ) {
+    ([tag, content, style] = nextSlice(tags, params));
+    const nextM = C([tag], content, style);
+    M = nextM(M);
+    if ( ! m ) {
+      m = M;
+    }
+  }
+  return m;
+}
+
+function hasSlice(tags, params) {
+
+}
+
+function nextSlice(tags, params) {
+
 }
